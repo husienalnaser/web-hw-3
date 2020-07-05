6f6df01a39942c299d8e04c5e7316c332c17b3a8
husienalnaser <husien_alnaser@hotmail.com>
2020-07-05 17:39:33 +0300

    first task

---
+++ Developer/js/scripts.js
@@ -0,0 +1,39 @@
+
+
+// let launguges=["python","javascrpt","html","css"]
+
+// function task(){
+//     let name= prompt("ما اسمك؟");
+//     let age = parseInt(prompt("كم عمرك؟"));
+//     let experience=parseInt(prompt("كم عدد سنوات الخبرة لديك؟"));
+    
+    
+
+
+
+
+
+
+//     console.log("1- "+ launguges[0])
+//     console.log("2- "+ launguges[1])
+//     console.log("3- "+ launguges[2])
+//     console.log("4- "+ launguges[3])
+
+//     let laung1= prompt("حدد رقم اللغة");
+//     let laung2= prompt("حدد رقم اللغة");
+
+//     if(age>25 && age<40 && experience>3 && laung1=="2" || laung2=="2" ){
+//        console.log("congrats "+name+" you got accepted")
+
+//     }else{
+//         console.log("sorry "+name+" you didnt get accepted")
+//     }
+    
+    
+
+// }
+
+
+
+// task();
+
