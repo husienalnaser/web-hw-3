a2adcc1e43f959d8004a2cf056bd5ec6adb4129d
husienalnaser <husien_alnaser@hotmail.com>
2020-07-06 15:55:55 +0300

    task3

--- Developer/js/scripts.js
+++ Developer/js/scripts.js
@@ -37,3 +37,60 @@
 
 // task();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function grade(){
+    let quizes = document.getElementById("quiz").value;
+let mid = document.getElementById("mid").value;
+let final = document.getElementById("final").value;
+let oral = document.getElementById("oral").value;
+
+let hun= quizes*1
+let hun1=mid*1
+let hun3=final*1
+let hun4=oral*1
+
+
+let grades=hun+hun1+hun3+hun4
+
+
+
+if(grades>89&&grades<101){
+    document.getElementById("head2").innerHTML="A"
+}
+if(grades>79&&grades<90){
+    document.getElementById("head2").innerHTML="B"
+}
+if(grades>69&&grades<80){
+    document.getElementById("head2").innerHTML="C"
+}
+if(grades>59&&grades<70){
+    document.getElementById("head2").innerHTML="D"
+}
+if(grades<60){
+    document.getElementById("head2").innerHTML="F"
+}
+
+document.getElementById("title").innerHTML=grades
+
+
+
+
+}
+
